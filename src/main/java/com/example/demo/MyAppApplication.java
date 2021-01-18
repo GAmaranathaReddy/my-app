aed9e0aab233e088454383919a9ca12e1111950a
IIITHAI&MLOutRearchProfessionals <44643853+IIITHAIOutRearchMonsters@users.noreply.github.com>
2021-01-18 23:14:02 +0800

    Update MyAppApplication.java

--- src/main/java/com/example/demo/MyAppApplication.java
+++ src/main/java/com/example/demo/MyAppApplication.java
@@ -9,6 +9,7 @@ public class MyAppApplication {
 	public static void main(String[] args) {
 		System.out.println("Main Method called");
 		SpringApplication.run(MyAppApplication.class, args);
+		System.out.println("Main Method Ended");
 	}
 
 }
